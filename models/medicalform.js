4eb44c1ad51a8c0a76ab90f9cdd17f1bcdb47969
tylertran0311 <karikkin@gmail.com>
2018-10-15 07:22:36 +1100

    Upload Medical Form to database

---
+++ models/medicalform.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+
+var MedicalFormSchema = mongoose.Schema({
+
+    // stores prescription for that student and the doctor that created the prescription
+    name: {
+        type: String
+    },
+    studentID:{
+        type: Number,
+        index: true
+    },
+    dob: {
+        type: String
+    },
+    postCode: {
+        type: Number
+    },
+    foneNo: {
+        type: Number
+    },
+    mdcNo: {
+        type: Number
+    },
+    description:{
+        type: String
+    }
+});
+
+var MedicalForm = (module.exports = mongoose.model("MedicalForm", MedicalFormSchema));
+
+module.exports.AddMedicalForm = function(newMedicalForm, callback) {
+    newMedicalForm.save(callback);
+};
--- routes/createMedicalForm.js
+++ routes/createMedicalForm.js
@@ -1,6 +1,7 @@
 var express = require("express");
 
 var router = express.Router();
+var MedicalForm = require("../models/medicalform");
 
 router.get("/", ensureAuthenticated, function(req, res) {
   console.log(999999)
@@ -9,7 +10,46 @@ router.get("/", ensureAuthenticated, function(req, res) {
 
 router.post("/", ensureAuthenticated, function(req, res) {
   validation(req, res);
+  createMedicalForm(req, res, newMedicalForm);
 });
+function validation(req,res) {
+  req.checkBody("name", "Name is required").notEmpty();
+  req.checkBody("dob", "Name is required").notEmpty();
+  req.checkBody("postCode", "Postcode is required").notEmpty();
+  req.checkBody("foneNo", "Phone number is required").notEmpty();
+  req.checkBody("mdcNo", "Medicare number is required").notEmpty();
+}
+function createMedicalForm(req, res, newMedicalForm) {
+  var name = req.body.name;
+  var dob = req.body.dob;
+  var postCode = req.body.postcode;
+  var foneNo = req.body.foneNo;
+  var mdcNo = req.body.mdcNo;
+  var desc = req.body.description;
+
+  var newMedicalForm = MedicalForm({
+    name: name,
+    dob: dob,
+    postCode: postCode,
+    foneNo: foneNo,
+    mdcNo: mdcNo,
+    desc: desc
+  });
+  
+  uploadMedicalForm(req, res, newMedicalForm);
+}
+
+function uploadMedicalForm(req, res, newMedicalForm) {
+  MedicalForm.AddMedicalForm(newMedicalForm, function(err, prescription){
+    if(err)
+        req.flash("error_msg", "Upload failed, please try again.");
+    else {
+        req.flash("success_msg", "Upload Success!");
+        console.log(prescription);
+    }
+    res.redirect("/");
+});
+}
 
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
@@ -18,20 +58,6 @@ function ensureAuthenticated(req, res, next) {
       req.flash("error_msg", "You are not logged in");
       res.redirect("/users/login");
   }
-}
-
-function validation(req,res) {
-  var name = req.body.name;
-  var dob = req.body.dob;
-  var postCode = req.body.postcode;
-  var foneNo = req.body.foneNo;
-  var mdcNo = req.body.mdcNo;
-
-  req.checkBody("name", "Name is required").notEmpty();
-  req.checkBody("dob", "Name is required").notEmpty();
-  req.checkBody("postCode", "Postcode is required").notEmpty();
-  req.checkBody("foneNo", "Phone number is required").notEmpty();
-  req.checkBody("mdcNo", "Medicare number is required").notEmpty();
 }
 
 module.exports = router;
